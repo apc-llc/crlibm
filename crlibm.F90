cdb81692a7b6aa2a6e35740367fbbe846e80c4fd
Dmitry Mikushin <dmitry@kernelgen.org>
2014-07-20 20:22:14 +0200

    Making crlibm module portable wrt non-PGI compilers

--- crlibm.F90
+++ crlibm.F90
@@ -2,63 +2,90 @@ module crlibm
 
 interface
 
-  attributes(host, device) function exp_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) &
+#endif
+  function exp_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: exp_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function log_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function log_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: log_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function log10_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function log10_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: log10_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function atan_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function atan_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: atan_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function tan_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function tan_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: tan_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function sin_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function sin_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: sin_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function cos_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function cos_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: cos_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function sinh_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function sinh_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: sinh_rn
   double precision, value :: x
   end function
 
-  attributes(host, device) function cosh_rn(x) bind(C)
+#ifdef CUDAFOR
+  attributes(host, device) & 
+#endif
+  function cosh_rn(x) bind(C)
   use iso_c_binding
   implicit none
   double precision :: cosh_rn
